e4020acf508955742b2a3584701b9db1d056e4d9
Raj Lamb <rajlamb2503@gmail.com>
2024-09-27 18:11:13 +0530

    Initial commit

---
+++ The-Last-Echo/src/main/java/com/NacroSoftTechnologies/The/Last/Echo/TheLastEchoApplication.java
@@ -0,0 +1,13 @@
+package com.NacroSoftTechnologies.The.Last.Echo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheLastEchoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheLastEchoApplication.class, args);
+	}
+
+}
---
+++ The-Last-Echo/src/test/java/com/NacroSoftTechnologies/The/Last/Echo/TheLastEchoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NacroSoftTechnologies.The.Last.Echo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheLastEchoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
